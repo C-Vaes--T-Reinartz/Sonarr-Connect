887f3d7cb1ba1011d5096dad8665b8eeb4d3eb15
Christian Vaes <c.vaes@maxxton.com>
2015-01-06 12:51:54 +0100

    add connection check to option page

--- options.js
+++ options.js
@@ -1,4 +1,34 @@
-// Saves options to chrome.storage
+/**
+ * 
+ * @author C.Vaes
+ */
+
+/**
+ * Test connection to sonarr server with api key. api/system/status call is used
+ */
+function test_connection() {
+var apiKey = document.getElementById('apiKey').value;
+var url = document.getElementById('url').value;
+var status = document.getElementById('connectionStatus');
+        $.ajax({
+        	url: url + 'api/system/status?apiKey=' + apiKey,
+         statusCode: {
+         200: function() {
+            status.textContent = 'Connected!';
+         },
+		 401: function() {
+			status.textContent = 'Credentials or url are not correct';
+			},
+		404: function() {
+		status.textContent = 'Sonarr is not running on this address';
+		}
+      }
+	  });	  
+}
+
+/**
+ * Save settings to chrome storage
+ */
 function save_options() {
   var apiKey = document.getElementById('apiKey').value;
   var url = document.getElementById('url').value;
@@ -15,8 +45,9 @@ function save_options() {
   });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
+/**
+ * Restore settings from chrome storage
+ */
 function restore_options() {
   // Use default value apiKey = '' and url = https://localhost.
   chrome.storage.sync.get({
@@ -27,6 +58,7 @@ function restore_options() {
     document.getElementById('url').value = items.url;
   });
 }
+//add listeners to buttons
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+document.getElementById('save').addEventListener('click',save_options);
+document.getElementById('testConnection').addEventListener('click',test_connection);

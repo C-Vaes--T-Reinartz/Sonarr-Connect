913b103612eb52f2b507e95d01eb23b64df3acd9
tomreinartz90 <tom@tomreinartz.com>
2015-01-17 19:40:28 +0100

    possible fix for background script

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
 var background = {
-standalone : function() {
+  standalone : function() {
     app.run();
-},
+  },
   getItemsInHistory : function () { 
     data = $.parseJSON(localStorage.getItem('wanted'));
     var num = data.totalRecords.toString();
@@ -19,5 +19,6 @@ background.getItemsInHistory();
 // set interval 
 chrome.alarms.create("getBackgroundData", {periodInMinutes: 5} );
 chrome.alarms.onAlarm.addListener(function(alarm) {
-  background.standalone.getItemsInHistory();
+  background.standalone();
+  background.getItemsInHistory();
 });
